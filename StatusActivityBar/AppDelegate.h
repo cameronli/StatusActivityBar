fa252536aa547392b23ac8dbd628c82221566074
Cameron <cameron@camerons-lion.local>
2013-05-16 13:38:01 +0800

    Initial Commit

---
+++ StatusActivityBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StatusActivityBar
+//
+//  Created by Cameron on 13-5-16.
+//  Copyright (c) 2013年 cm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
